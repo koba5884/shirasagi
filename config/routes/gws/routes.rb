78538015296d5d187d5bada490f33fd8e2d3e594
ka <ka.kaosf@gmail.com>
2015-03-25 17:37:03 +0900

    Set GWS routing

---
+++ config/routes/gws/routes.rb
@@ -0,0 +1,43 @@
+SS::Application.routes.draw do
+  get '..g:group/', to: 'gws/portal#index', as: :gws_main
+
+  gws "gws" do
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :roles do
+      get :delete, on: :member
+    end
+  end
+  # TODO integrate / (as portal) and gws/roles
+
+  gws "schedule" do
+    resources :plans do
+      get :delete, on: :member
+    end
+    get "calendar" => "calendar#index"
+  end
+
+  # WIP
+  #gws "reservation" do
+  #  resources :plans
+  #end
+
+  gws "board" do
+    resources :topics do
+      get :delete, on: :member
+      namespace :parent, path: ":parent_id", parent_id: /\d+/ do
+        resources :comments, except: [:index], controller: '/board/comments' do
+          get :delete, on: :member
+        end
+      end
+    end
+  end
+
+  gws "share" do
+    resources :files do
+      get :view, on: :member
+      get :thumb, on: :member
+      get :download, on: :member
+      get :delete, on: :member
+    end
+  end
+end
